47a92e89264636b849084a6c17fc642bf2b0c01a
DzedajMuharem <mujanovich100@gmail.com>
2022-11-08 13:28:14 +0100

    provera filmovi

--- domaci/Filmovi(2.lekcija)/app.js
+++ domaci/Filmovi(2.lekcija)/app.js
@@ -1,54 +1,57 @@
 let allTotal = 0;
+let nameTotal = "";
 
 function addToTotal(element) {
   let mainEl = element.closest(".kartica");
-  //   console.log(mainEl);
-  let name = mainEl.querySelector("h1").innerText;
-  //   console.log(name);
+  // console.log(mainEl);
   let price = mainEl.querySelector("p span").innerText;
-  //   console.log(price);
-  element;
+  // console.log(price);
   let total = document.querySelector("#total");
-  let odgledanih = document.querySelector("#odgledanih");
   let deleteBtn = mainEl.querySelector(".btn-delete");
+  let name = mainEl.querySelector("h1").innerText;
+  let odgledanih = mainEl.querySelector("#odgledanih");
 
-  price = price.substring(1, price.length);
+  // Total cena
+  price = price.substring(1, price.lenght);
   price = parseInt(price);
   allTotal += price;
-  //   console.log(allTotal);
-  total.innerHTML = `Ukupno potroseno novca: $${allTotal}`;
+  total.innerText = `Ukupno potroseno novca: $${allTotal}`;
+  if (allTotal > 0) {
+    total.style.visibility = "visible";
+  }
+
+  // izgled kartice
   mainEl.style.background = "blue";
-  mainEl.style.opacity = "0.8";
-
-  element.innerText = "Dodato";
-  element.setAttribute("disabled", "");
-
+  mainEl.style.opacity = "0.7";
   deleteBtn.style.display = "block";
+  element.setAttribute("disabled", "");
 }
 
 function deleteBtn(element) {
   let mainEl = element.closest(".kartica");
-  let total = document.querySelector("#total");
+  let deleteBtn = mainEl.querySelector(".btn-delete");
+  let dodajBtn = mainEl.querySelector(".btn");
   let price = mainEl.querySelector("p span").innerText;
-  let karta = document.querySelector(".kartica");
-  let name = mainEl.querySelector("h1");
-  let mainBtn = mainEl.querySelector(".btn");
+  let total = document.querySelector("#total");
 
-  mainEl.innerHTML = ` <img src="./img/carrot.png" alt="" />
-<div class="body-kartice">
-  <h2>Ocena: 7.2</h2>
-  <h1>Barbarian</h1>
-  <button onclick="addToTotal(this)">+ Dodaj</button>
-  <p>Cena: <span>$10</span></p>
-  <button onclick="deleteBtn(this)" class="btn-delete">X</button>
-</div>`;
+  // console.log(mainEl);
+
+  // izgled kartice na staro
+
   mainEl.style.background = "red";
   mainEl.style.opacity = "1";
-  element.style.display = "none";
-  price = price.substring(1, price.length);
-  price = parseInt(price);
-  allTotal -= price;
-  total.innerText = `Ukupno potroseno novca: $ ${allTotal}`;
+  deleteBtn.style.display = "none";
+  dodajBtn.removeAttribute("disabled");
 
-  mainBtn.removeAttribute("disabled");
+  // total update
+  price = price.substring(1, price.lenght);
+  price = parseInt(price);
+
+  allTotal -= price;
+
+  if (allTotal === 0) {
+    total.style.visibility = "hidden";
+  } else {
+    total.innerText = `Ukupno potroseno novca: $${allTotal}`;
+  }
 }
